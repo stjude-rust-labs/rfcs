d935ef9a2c1d6dae0cdf8eeeb5975ff2bfedb9ca
St. Jude Cloud <support@stjude.cloud>
2025-12-02 20:35:44 +0000

    Deployed from rfc: sprocket test (#1)
    * Create 0001-sprocket-test.md
    
    * Update ci-build.sh
    
    * Update 0000-placeholder.md
    
    * docs: TODO -> REVIEWERS
    
    * docs: link to template
    
    * revise: elaborate on pytest-workflow
    
    * feat: more prior art
    
    * feat: elaborate on some other future possibilities
    
    * feat: test filtering
    
    * feat: future possibility: caching
    
    * feat: custom test details
    
    * feat: more discussion of the custom test design
    
    * chore: typos, rephrasing awkward clauses, etc
    
    * chore: review feedback
    
    * revise: "test" -> "assertion"
    
    * chore: review feedback
    
    elaborating on pytest-workflow and mentioning adoption by WDL spec
    
    * ci: remove bad key 4793746305dade79a8a42b059192a7c389016dd8

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e3f913f5.js',
+                path_to_root + 'searchindex-d6a1e0c8.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
