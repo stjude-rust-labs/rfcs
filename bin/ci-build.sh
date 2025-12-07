717f8ab03e7ff7d34a2d6758544a9c91e2c92845
Ari Frantz <ari.frantz@stjude.org>
2025-12-07 11:58:16 -0500

    fix: build all branches except 'gh-pages'
    only draft RFCs with branch names starting with `rfcs/` were being built

--- bin/ci-build.sh
+++ bin/ci-build.sh
@@ -42,7 +42,7 @@ git fetch --all
 BRANCHES=()
 while IFS= read -r line; do
     BRANCHES+=( "$line" )
-done < <( git branch --list --all | sed 's,\*,,g' | xargs -n1 | grep "remotes/origin" | sed 's,remotes/origin/,,g' | sort | uniq | grep -e 'main' -e 'rfcs/' )
+done < <( git branch --list --all | sed 's,\*,,g' | xargs -n1 | grep "remotes/origin" | sed 's,remotes/origin/,,g' | sort | uniq | grep -v 'gh-pages' )
 
 echo "== Creating Drafts File =="
 printf "# Drafts\n\n" > "${DRAFTS_FILE}"
