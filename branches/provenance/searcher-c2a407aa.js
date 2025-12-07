5a8fdb3e5269c08813d96164c848f0f7f5ebed2d
St. Jude Cloud <support@stjude.cloud>
2025-12-07 17:28:54 +0000

    Deployed from fix: HEAD e5c79c3903b708d1edbd9e59ef9d30e002650d43

--- branches/provenance/searcher-c2a407aa.js
+++ branches/provenance/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bfef3c60.js',
+                path_to_root + 'searchindex-e8468fbe.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f0bc3eb6.js',
+                path_to_root + 'searchindex-1150dcfe.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
