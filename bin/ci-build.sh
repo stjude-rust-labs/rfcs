b21a5e48ed23076d270b7efb986eb504407c3343
Ari Frantz <ari.frantz@stjude.org>
2025-12-07 14:14:50 -0500

    fix: build main first

--- bin/ci-build.sh
+++ bin/ci-build.sh
@@ -55,14 +55,16 @@ for CURRENT_BRANCH in "${BRANCHES[@]}"; do
   fi
 done
 
+# build `main` first. Otherwise it will clobber other branches
+build main book/ "${DRAFTS_FILE}"
+
 # Loop through rfc branches, build and copy each to output dir
 echo "== Build RFC Branches =="
 for CURRENT_BRANCH in "${BRANCHES[@]}"; do 
-  BRANCH_DIR="${BOOK_DIR}/branches/${CURRENT_BRANCH}"
-  if [[ "${CURRENT_BRANCH}" == "main" ]]; then
-    BRANCH_DIR="${BOOK_DIR}/"
+  if [[ "${CURRENT_BRANCH}" != "main" ]]; then
+    BRANCH_DIR="${BOOK_DIR}/branches/${CURRENT_BRANCH}"
+    build "${CURRENT_BRANCH}" "${BRANCH_DIR}" "${DRAFTS_FILE}"
   fi
-  build "${CURRENT_BRANCH}" "${BRANCH_DIR}" "${DRAFTS_FILE}"
 done
 
 rm ${DRAFTS_FILE}
