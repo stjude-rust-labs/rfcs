6cc4ec4ae98a8c92c36607844feb6d5a950ba4c8
St. Jude Cloud <support@stjude.cloud>
2025-12-07 17:06:10 +0000

    Deployed from ci: build on other branches 8f1385ebebd5c9adfc4367f6f24e67ceb3139ffd

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d6a1e0c8.js',
+                path_to_root + 'searchindex-f0bc3eb6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
